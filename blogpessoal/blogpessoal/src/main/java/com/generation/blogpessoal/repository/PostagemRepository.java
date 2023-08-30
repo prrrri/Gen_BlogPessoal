290d464df974c02871b944f9bce51f6be1183858
Priscila Igarashi <132402519+prrrri@users.noreply.github.com>
2023-08-30 16:15:48 -0300

    atividade CRUD back-end blog-pessoal 2

--- blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,14 @@
 package com.generation.blogpessoal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-import com.generation.blogpessoal.model.Postagem;abstract
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	
 }
