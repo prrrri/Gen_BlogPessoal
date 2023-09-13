45ec02838dcfb8213fab533702c0cffd80f29402
Priscila Igarashi <132402519+prrrri@users.noreply.github.com>
2023-09-13 15:29:38 -0300

    ecossistema de segurança

--- blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -20,56 +20,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig {
-	
-	@Autowired
-	private JwtAuthFilter authFilter;
-	
-	@Bean
-	UserDetailsService userDetailsService() {
-		return new UserDetailsServiceImpl();
-	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService());
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		return authenticationProvider;
-	}
-	
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-	throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
-	
-	@Bean
-	SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-		
-		http
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().csrf().disable()
-		.cors();
-		
-		http
-		.authorizeHttpRequests((auth) -> auth
-		.requestMatchers("/usuarios/logar").permitAll()
-		.requestMatchers("/usuarios/cadastrar").permitAll()
-		.requestMatchers("/error/**").permitAll()
-		.requestMatchers(HttpMethod.OPTIONS).permitAll()
-		.anyRequest().authenticated())
-.authenticationProvider(authenticationProvider())
-.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-.httpBasic();
 
-return http.build();
-		
-	}
+    @Autowired
+    private JwtAuthFilter authFilter;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .cors();
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/usuarios/logar").permitAll()
+                        .requestMatchers("/usuarios/cadastrar").permitAll()
+                        .requestMatchers("/error/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated())
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic();
+
+        return http.build();
+
+    }
 
 }
