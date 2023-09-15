23734a82c2feb3d345c4f09ef9f95ba6d7e9e1fb
Priscila Igarashi <132402519+prrrri@users.noreply.github.com>
2023-09-15 17:14:52 -0300

    testes unitários junit

--- blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -42,6 +42,23 @@ public class Usuario {
 	@OneToMany (fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+	
+	
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto,
+			List<Postagem> postagem) {
+		Id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.postagem = postagem;
+	}
+	
+	
+
+	public Usuario() {}
+
 
 	public Long getId() {
 		return Id;
@@ -90,9 +107,6 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
-	// Getters and Setters
-	
 	
 
 }
